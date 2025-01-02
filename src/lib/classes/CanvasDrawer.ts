d22e08fefb7e5631feeeea4c29094722d26c74ca
Diego <diesalasmart@gmail.com>
2025-01-01 22:51:44 -0500

    refactor: add wrapper function for canvas drawing

--- src/lib/classes/CanvasDrawer.ts
+++ src/lib/classes/CanvasDrawer.ts
@@ -26,60 +26,42 @@ export default class CanvasDrawer {
 		// Clear static canvas
 		this.clearCanvas(this.canvasStatic);
 
-		// Apply translation
-		this.canvasStatic.context.save();
-		const { x: offsetX, y: offsetY } = this.canvasStore.getOffset();
-		this.canvasStatic.context.translate(offsetX, offsetY);
+		this.drawCanvasCallback(this.canvasStatic, () => {
+			// Draw background
+			this.canvasStatic.context.fillStyle = '#f0f0f0';
+			this.canvasStatic.context.fillRect(-10000, -10000, 20000, 20000);
 
-		// Draw background
-		this.canvasStatic.context.fillStyle = '#f0f0f0';
-		this.canvasStatic.context.fillRect(-10000, -10000, 20000, 20000);
-
-		// Draw shapes
-		for (const shape of this.canvasStore.getShapes()) {
-			this.shapeEntity.drawShape(shape);
-		}
-
-		// Restore translation
-		this.canvasStatic.context.restore();
+			// Draw shapes
+			for (const shape of this.canvasStore.getShapes()) {
+				this.shapeEntity.drawShape(shape);
+			}
+		});
 	}
 
 	drawCanvasInteractive() {
 		// Clear interactive canvas
 		this.clearCanvas(this.canvasInteractive);
 
-		// Apply translation
-		this.canvasInteractive.context.save();
-		const { x: offsetX, y: offsetY } = this.canvasStore.getOffset();
-		this.canvasInteractive.context.translate(offsetX, offsetY);
-
-		// Draw current shape
-		const currentShape = this.canvasStore.getCurrentShape();
-		if (currentShape) {
-			this.shapeEntity.selectShape(currentShape);
-		}
-
-		// Restore translation
-		this.canvasInteractive.context.restore();
+		this.drawCanvasCallback(this.canvasInteractive, () => {
+			// Draw current shape
+			const currentShape = this.canvasStore.getCurrentShape();
+			if (currentShape) {
+				this.shapeEntity.selectShape(currentShape);
+			}
+		});
 	}
 
 	drawing(event: MouseEvent) {
-		const coordsMouse = this.getMousePosition(event);
-
 		// Clear interactive canvas
 		this.clearCanvas(this.canvasInteractive);
 
-		// Apply translation
-		this.canvasInteractive.context.save();
-		const { x: offsetX, y: offsetY } = this.canvasStore.getOffset();
-		this.canvasInteractive.context.translate(offsetX, offsetY);
+		this.drawCanvasCallback(this.canvasInteractive, () => {
+			// Draw shape to interactive canvas
+			const coordsMouse = this.getMousePosition(event);
+			const coordsStart = this.canvasStore.getStartPosition();
 
-		// Draw shape to interactive canvas
-		const coordsStart = this.canvasStore.getStartPosition();
-		this.shapeEntity.drawCoords(coordsStart, coordsMouse);
-
-		// Restore translation
-		this.canvasInteractive.context.restore();
+			this.shapeEntity.drawCoords(coordsStart, coordsMouse);
+		});
 	}
 
 	stopDrawing(event: MouseEvent) {
@@ -98,16 +80,10 @@ export default class CanvasDrawer {
 		// Clear interactive canvas
 		this.clearCanvas(this.canvasInteractive);
 
-		// Apply translation
-		this.canvasStatic.context.save();
-		const { x: offsetX, y: offsetY } = this.canvasStore.getOffset();
-		this.canvasStatic.context.translate(offsetX, offsetY);
-
-		// Draw shape to static canvas
-		this.shapeEntity.drawShape(shape);
-
-		// Restore translation
-		this.canvasStatic.context.restore();
+		this.drawCanvasCallback(this.canvasStatic, () => {
+			// Draw shape to static canvas
+			this.shapeEntity.drawShape(shape);
+		});
 	}
 
 	startEditing(event: MouseEvent, shape: Shape) {
@@ -120,32 +96,22 @@ export default class CanvasDrawer {
 		const { x, y } = this.getMousePosition(event);
 
 		const { x: startX, y: startY } = this.canvasStore.getStartPosition();
-		console.log('[DIEGO] start', startX, startY);
-		console.log('[DIEGO] here', x, y);
-		console.log('[DIEGO] nodeSelected', this.nodeSelected);
 	}
 
 	click(event: MouseEvent) {
-		// Apply translation
-		this.canvasInteractive.context.save();
-
-		const { x: offsetX, y: offsetY } = this.canvasStore.getOffset();
-		this.canvasInteractive.context.translate(offsetX, offsetY);
-
 		// Clear interactive canvas
 		this.clearCanvas(this.canvasInteractive);
 
-		// Print selected shape
-		const shapeSelected = this.hasSelectedShape(event);
-		if (shapeSelected) {
-			this.canvasStore.setCurrentShape(shapeSelected);
-			this.shapeEntity.selectShape(shapeSelected);
-		} else {
-			this.canvasStore.setCurrentShape(null);
-		}
-
-		// Restore translation
-		this.canvasInteractive.context.restore();
+		this.drawCanvasCallback(this.canvasInteractive, () => {
+			// Print selected shape
+			const shapeSelected = this.hasSelectedShape(event);
+			if (shapeSelected) {
+				this.canvasStore.setCurrentShape(shapeSelected);
+				this.shapeEntity.selectShape(shapeSelected);
+			} else {
+				this.canvasStore.setCurrentShape(null);
+			}
+		});
 	}
 
 	hasSelectedShape(event: MouseEvent) {
@@ -174,5 +140,18 @@ export default class CanvasDrawer {
 		const currentY = event.clientY - canvasRect.top - offsetY;
 
 		return { x: currentX, y: currentY };
+	}
+
+	private drawCanvasCallback(canvas: CanvasInstance, callback: () => void) {
+		// Apply translation
+		canvas.context.save();
+		const { x: offsetX, y: offsetY } = this.canvasStore.getOffset();
+		canvas.context.translate(offsetX, offsetY);
+
+		// Custom callback
+		callback();
+
+		// Restore translation
+		canvas.context.restore();
 	}
 }
