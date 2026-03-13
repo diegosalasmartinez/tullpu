3aa090071bb3dc3cae01b53e2e9f120cc5726321
Diego <diesalasmart@gmail.com>
2026-03-13 01:21:44 -0500

    chore: add circle icon

---
+++ src/lib/icons/Circle.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	let { className = '' }: { className?: string } = $props();
+</script>
+
+<svg
+	aria-hidden="true"
+	focusable="false"
+	role="img"
+	viewBox="0 0 24 24"
+	class={className}
+	fill="none"
+	stroke-width="2"
+	stroke="currentColor"
+	stroke-linecap="round"
+	stroke-linejoin="round"
+>
+	<g stroke-width="1.5">
+		<path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+		<circle cx="12" cy="12" r="8"></circle>
+	</g>
+</svg>
